60c767d1cf3ff98450d5df269af5371e69150a79
Nicolas Michel <nicolas.michel.lava@gmail.com>
2014-10-31 16:57:33 +0100

    * Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
-                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [prismatic/dommy "0.1.3"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
