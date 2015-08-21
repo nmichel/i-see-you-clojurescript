971588663ea5f6a4f47f91baa35e17e57e9c6a09
Nicolas Michel <nicolas.michel.lava@gmail.com>
2015-08-21 10:31:20 +0200

    * Use figwheel.

--- project.clj
+++ project.clj
@@ -3,17 +3,27 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [prismatic/dommy "1.0.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-figwheel "0.3.7"]]
 
   :source-paths ["src"]
 
+  :figwheel {
+             :http-server-root "public" ;; this will be in resources/
+             :server-port 3449          ;; default
+             :server-ip   "192.168.232.129"     ;; default
+             }
+  
   :cljsbuild {
     :builds [{:id "cljs-intro"
               :source-paths ["src"]
+              :figwheel {
+                         :websocket-host "192.168.232.129"}
+              
               :compiler {
                 :output-to "cljs_intro.js"
                 :output-dir "out"
